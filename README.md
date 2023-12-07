6e035fbfd6321e578141504b9394d1ef9867f990
d-kd <97709041+jT2a@users.noreply.github.com>
2023-12-07 18:37:17 +1100

    Init

---
+++ README.md
@@ -0,0 +1,24 @@
+![alt text](idk.png "I don't know")
+
+# ¯\\_(ツ)_/¯ I Don't Know ASCII (IDK) CLI
+## Easily generate the "I Don't Know" ascii from command line
+
+Ever wanted to get fast access to the Internet famous "I don't know" ASCII?
+Here it is in command line format for fast and easy access. Just install and run the $idk command and 🪄✨
+
+## Features
+
+- 1 x ¯\\_(ツ)_/¯ I don't know ASCII
+
+## Installation
+
+```sh
+npm i -g idkcli
+```
+
+
+## Usage
+
+```sh
+idk
+```
---
+++ bin/app.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+// const chalk = require('chalk');
+import chalk from 'chalk'
+import clipboardy from 'clipboardy'
+
+clipboardy.writeSync('¯\\_(ツ)_/¯')
+
+console.log(chalk.green.bold('---------------------------------'))
+console.log(chalk.yellow.bold('⚡️⚡️✨  Generating Magic  🪄✨⚡️⚡️'))
+console.log(chalk.green.bold('---------------------------------'))
+console.log(chalk.blue.bold('\n\n          ¯\\_(ツ)_/¯ \n\n'))
+console.log(chalk.green.bold('---------------------------------'))
+console.log(chalk.yellow.bold('⚡️⚡️✨ Your ASCII is ready ✨⚡️⚡️'))
+console.log(chalk.yellow.bold('⚡️⚡️✨ Copied to clipboard ✨⚡️⚡️'))
+console.log(chalk.green.bold('---------------------------------'))
